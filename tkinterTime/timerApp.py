65f51fd1ccdfdb08cd45b31d73499f3041e678ae
rcd1 <70033436+rcd1@users.noreply.github.com>
2026-05-29 23:44:48 -0400

    have a basic idea of th ebuttons, need to figure out how to shape the fram so the anchors work if that's how they do yugi—

--- tkinterTime/timerApp.py
+++ tkinterTime/timerApp.py
@@ -5,13 +5,29 @@ class Application(tk.Frame):
         tk.Frame.__init__(self, master)
         self.grid()
         self.createWidgets()
-    
+    # This isn't a real part of tkinter, just something for us to separate the things
     def createWidgets(self):
-        self.quitButton = tk.Button(self, text='Quit',
-                                    command=self.quit)
-        self.quitButton.grid()
-        self.startButton = tk.Button(self,text='Start',command=self.quit)
+        # Starts the timer
+        self.startButton = tk.Button(self,text='Start',command=self.startTimer, anchor='w')
         self.startButton.grid()
+        # Pauses timer
+        self.pauseButton = tk.Button(self,text='Pause',command=self.pauseTimer, anchor='center')
+        self.pauseButton.grid()
+        # Resets Timer
+        self.resetButton = tk.Button(self,text='Reset',command=self.resetTimer, anchor='e')
+        self.resetButton.grid()
+        # Quits Application
+        self.quitButton = tk.Button(self, text='Quit', command=self.quit, anchor='s')
+        self.quitButton.grid()
+
+    def startTimer(self):
+        print("timer started!")
+    
+    def pauseTimer(self):
+        print("timer paused ;-;")
+
+    def resetTimer(self):
+        print("timer reset")
 
 app = Application()
 app.master.title('Sample application')
